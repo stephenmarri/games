af44a773b4ea579c79f059726ed2f2fa0f24f45b
Marri <stephenmarri@kpmg.com>
2020-05-03 20:49:08 +0530

    third commit for whackamole

--- whackamole/script.js
+++ whackamole/script.js
@@ -15,6 +15,9 @@ var timerID = setInterval(function(){
         document.querySelector('#play__text').textContent='Play Again';
         startButton.style.background='#27ae60';    
         startButton.style.fontWeight='Initial'; 
+        //modal-- game over
+        modal.style.display = "block";
+        document.querySelector('#modal--score').textContent=score;
     }
 
     timer__html.textContent=timeLeft;
@@ -73,3 +76,29 @@ startButton.addEventListener('click',()=>{
     }   
 
 })
+
+
+//modal
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
