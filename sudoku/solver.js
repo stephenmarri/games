bf372980c5d89303323b937a889be82618b3d24f
Marri Stephen <stephenmarri7@gmail.com>
2020-08-17 07:11:43 +0530

    created isTheBoardSolved Function

--- sudoku/solver.js
+++ sudoku/solver.js
@@ -1,7 +1,7 @@
 class Solver {
     watch = true;
     speed = 200;
-    requestStop = false;    
+    requestStop = false;
 
     board;
     originalBoard;
@@ -14,7 +14,7 @@ class Solver {
     boardBoxes;
 
     questionsCount;
-    solvedCount=0;
+    solvedCount = 0;
     isBoardSolved = false;
 
     emptiesObject = {};
@@ -30,13 +30,13 @@ class Solver {
 
         this.init3arrays()
         this.questionsCount = this.findQuestionsCount();
-        this.solvedCount = 0;        
+        this.solvedCount = 0;
     }
 
-    startSolving(){
+    startSolving() {
         this.initEmptiesObject()
         this.singlesFinder()
-        if(!this.isBoardSolved) this.backTracking()
+        if (!this.isTheBoardSolved("Singles")) this.backTracking()        
     }
 
     initEmptiesObject() {
@@ -71,7 +71,7 @@ class Solver {
 
     //################################ Singles Finder START
 
-    // the below function is not being used and is replaced by singlesFinder
+    //? the below function is not being used and is replaced by singlesFinder
     createSinglesObject() {
         let singlesCounter = 0;
 
@@ -96,7 +96,7 @@ class Solver {
         if (this.solvedCount == this.questionsCount) {
             this.isBoardSolved = true;
             console.log("Board Solved: createSinglesObject attempt 1");
-        }else{
+        } else {
             this.singlesFinder()
         }
 
@@ -124,25 +124,21 @@ class Solver {
             if (valuesFilledCounter > 0) {
                 this.solvedCount += valuesFilledCounter;
                 this.writeToBoard(this.singlesObject)
-                view.printBoard(this.board)                
+                view.printBoard(this.board)
             }
         } while (valuesFilledCounter > 0);
 
         // is board solved
-        if (this.solvedCount == this.questionsCount) {
-            this.isBoardSolved = true;
-            console.log("Singles: board solved");
-            //validate the board
-            let isSolutionValid = this.boardValidation()
-            if(isSolutionValid) console.log('Singles: solution is a valid sudoku');
-            alert('Yo!!! Board Solved')
-        }
-        console.log(`Singles: ${this.solvedCount} filled out of ${this.questionsCount}`);
+        console.log(`Singles : ${this.solvedCount} filled out of ${this.questionsCount}`);        
     }
 
     findSinglesFromObject(obj) {
         let valueFilled = 0;
-        let { row, col, box } = obj;
+        let {
+            row,
+            col,
+            box
+        } = obj;
         let isSingle = this.checkIfSingle(row, col, box);
         if (isSingle.length == 1) {
             valueFilled = isSingle[0];
@@ -158,7 +154,9 @@ class Solver {
     }
 
     checkIfSingle(row, col, box) {
-        let possible = Array.from({ length: boardSize }, (val, idx) => idx + 1)
+        let possible = Array.from({
+            length: boardSize
+        }, (val, idx) => idx + 1)
         let rowValues = [...new Set(this.boardRows[row])]
         let colValues = [...new Set(this.boardColumns[col])]
         let boxValues = [...new Set(this.boardBoxes[box])]
@@ -171,88 +169,92 @@ class Solver {
     }
 
     //################################ Singles Finder END
-    
+
     //################################ back tracking START
-    async backTracking(){        
+    async backTracking() {
 
         let bTquestions = this.createBackTrackingObject()
-        console.log(`backTrackign: remaining empty items are: ${bTquestions}`);
+        console.log(`Back Tracking : empty items left - ${bTquestions}`);
         let beforeBackTrackBoard = copyBoard(this.board);
 
         let i = 0;
         let iterations = 0;
-        while(i < bTquestions){            
-            let obj = this.backTrackObject[i]                        
-            let {row, col, box, possibleValues, possibleIdx} = obj;            
+        while (i < bTquestions) {
+            let obj = this.backTrackObject[i]
+            let {
+                row,
+                col,
+                box,
+                possibleValues,
+                possibleIdx
+            } = obj;
 
             let isFilled = false;
-            while(possibleIdx < possibleValues.length){
+            while (possibleIdx < possibleValues.length) {
                 let value = possibleValues[possibleIdx]
                 let isValueValid = this.checkIfValidValue(row, col, box, value)
                 possibleIdx++;
                 this.backTrackObject[i].possibleIdx = possibleIdx;
-                if(isValueValid){
-                    this.backTrackObject[i].currentValue = value   
-                    isFilled = true       
+                if (isValueValid) {
+                    this.backTrackObject[i].currentValue = value
+                    isFilled = true
                     break;
                 }
             }
 
             //if no value filled for this 'i'
-            if(!isFilled){
-                if(i == 0 ){
-                    console.log('backTracking: solution cannot be found');
+            if (!isFilled) {
+                if (i == 0) {
+                    this.isTheBoardSolved("backtracking")
                     return;
-                }else{
+                } else {
                     this.backTrackObject[i].possibleIdx = 0;
                     this.backTrackObject[i].currentValue = 0;
                     i -= 2;
                 }
             }
-            
+
             this.writeToBoard(this.backTrackObject)
-            view.printBoard(this.board)   
+            view.printBoard(this.board)
             //if user requests to stop then abort
-            if(this.requestStop) return;
+            if (this.requestStop) return;
 
             // all things related to speeed and watch
-            if(this.watch){
-                let reduction = parseInt(iterations/100)*10
+            if (this.watch) {
+                let reduction = parseInt(iterations / 100) * 10
                 reduction = reduction >= this.speed ? 0 : reduction;
-                await sleep(this.speed - reduction )   
+                await sleep(this.speed - reduction)
             }
 
 
-            i++; iterations++;
+            i++;
+            iterations++;
         }
 
 
         // final steps
-        console.log('iteration done: ', iterations);
-        if(this.boardValidation()){
-            console.log('backTracking Complete: board Solved');
-            alert('Yo!!! Board Solved')
-        }else{
-            console.log('backTracking Complete: no solution found');
-        }
-        
+        this.solvedCount += i;
+        console.log('Back Tracking : iterations done - ', iterations);
+        console.log(`Back Tracking : ${i} filled out of ${bTquestions}`);        
+        // final validation and exit with result
+        this.isTheBoardSolved("backtracking")
     }
 
-    checkIfValidValue(row,col,box,value){
+    checkIfValidValue(row, col, box, value) {
         let isValid = true;
         let rowValues = [...new Set(this.boardRows[row])]
         let colValues = [...new Set(this.boardColumns[col])]
         let boxValues = [...new Set(this.boardBoxes[box])]
-        if(rowValues.includes(value) || colValues.includes(value) || boxValues.includes(value)) isValid = false;        
+        if (rowValues.includes(value) || colValues.includes(value) || boxValues.includes(value)) isValid = false;
 
         return isValid;
     }
 
-    createBackTrackingObject(){
+    createBackTrackingObject() {
         let counter = 0;
-        for(let idx in this.emptiesObject){
+        for (let idx in this.emptiesObject) {
             let obj = this.emptiesObject[idx];
-            if(!obj.solved){
+            if (!obj.solved) {
                 this.backTrackObject[counter] = obj;
                 counter++;
             }
@@ -282,9 +284,33 @@ class Solver {
         return parseInt(Math.pow(this.boardSize, 2)) - this.board.reduce((a, b) => a + (b.filter(x => x > 0).length), 0)
     }
 
-    boardValidation(){
-        let resultValidator = new Validate(this.board,this.boardSize);
+    boardValidation() {
+        let resultValidator = new Validate(this.board, this.boardSize);
         return resultValidator.runTests()
+    }
+
+    isTheBoardSolved(stage) {
+        if (this.solvedCount == this.questionsCount) {
+            this.isBoardSolved = true;
+            console.log(stage,": board solved");
+            
+            //validate the board
+            let isSolutionValid = this.boardValidation()
+            if (isSolutionValid){
+                console.log('Board solved.🤘');
+                alert('Board Solved. 🤘')
+            }else{
+                console.log('Solver process complete. But board is invalid somehow.Sorry 😥');
+                alert('Process complete, but solution is invalid. Sorry 😥')
+            }
+
+        } else if (stage.toString().replace(' ', '').toLowerCase() == "backtracking") {
+            this.isBoardSolved = false;
+            console.log('Solver process complete. But could not solve the board.😣');
+            alert(`Process complete, but couldn't solve the board. Sorry 😣`)
+        }
+
+        return this.isBoardSolved
     }
 
 }
